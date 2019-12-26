95374c43fb0a91f1b543ef422eed2b04db240c70
Vojtech Juranek <vojtech.juranek@gmail.com>
2019-12-27 00:33:24 +0100

    Add counter implementation

--- jepsen.jgroups.raft/src/jepsen/jgroups/raft.clj
+++ jepsen.jgroups.raft/src/jepsen/jgroups/raft.clj
@@ -6,16 +6,48 @@
              [db :as db]
              [tests :as tests]]
             [jepsen.control.util :as cu]
-            [jepsen.os.debian :as debian]))
+            [jepsen.os.debian :as debian])
+  (:import
+    (org.jgroups JChannel)
+    (org.jgroups.blocks.atomic Counter)
+    (org.jgroups.raft.blocks CounterService)))
+
+
+(defn provide-name
+  []
+  (throw (Exception. "You have to provice node name via -name argument.")))
+
+(def raft-config "raft.xml")
+
+(defn jchannel
+  [props, name]
+  (-> (JChannel. props) (.name name)))
+
+(defn jcnt
+  [jch]
+  (-> (CounterService. jch) (.raftId (.name jch)) (.replTimeout 5000) (.getOrCreateCounter "counter" 0)))
+
+(defn join-channel
+  [jch]
+  (.connect jch "counters"))
+
+(defn start-counter
+  [props, name]
+  (let [jch (jchannel props name),
+        cnt (jcnt jch)]
+    (join-channel jch)
+    cnt))
 
 (defn counter
   []
   (reify db/DB
          (setup! [_ test node]
-                 (info node "installing jgroups counter"))
+                 (info node "Starting JGroups counter")
+                 (start-counter raft-config node))
 
          (teardown! [_ test node]
-                    (info node "tearing down jgroups counter"))))
+                    ;; TODO: stop jch here
+                    (info node "Stopping JGroups counter - no-op for now"))))
 
 (defn counter-test
   [opts]
